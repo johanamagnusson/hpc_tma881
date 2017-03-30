9833f5ca619327e4889d1ef1f012af9b2a3e2f96
Andreas Magnusson <andreas.magnusson90@gmail.com>
2017-03-30 17:13:47 +0200

    worked on time on assignment 1

---
+++ assignment1/time/sum.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+
+#define NUMBER_OF_ITERATIONS 1000
+
+int main()
+{
+    double diffTime;
+
+    clock_t start;
+    clock_t end;
+
+    const unsigned long int num = 1000000000;
+
+    unsigned long int sum;
+    unsigned long int i;
+    unsigned long int j;
+    double totalTime;
+    double averageTime;
+    
+    totalTime = 0.0;
+    for (j = 0; j < NUMBER_OF_ITERATIONS; j++)
+    {
+        sum = 0;
+        start = clock();
+        for (i = 1; i <= num; i++)
+        {
+            sum += i;
+        }
+        end = clock();
+        diffTime = ((end - start) / (double) CLOCKS_PER_SEC) * 1000;
+        totalTime += diffTime;
+    }
+    
+    averageTime = totalTime / NUMBER_OF_ITERATIONS;
+    printf("Sum = %lu\n", sum);
+    printf("Time taken: %.8f ms\n", averageTime);
+
+    return 0; 
+    
+}
