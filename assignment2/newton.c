75d6291223c00675f3fe3ad6418409e5590a6cff
Rickard Johansson <rickard.s.m.johansson@gmail.com>
2017-04-20 16:32:15 +0200

    implemented newton function

--- assignment2/newton.c
+++ assignment2/newton.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <argp.h>
-
-
+#include <complex.h>
+#include <math.h>
 
 struct arguments
 {
@@ -45,6 +45,29 @@ parse_opt(int key, char *arg,
     return 0;
 }
 
+struct newton{
+    int iterations;
+    int root; //might be represented as the complex root:W
+};
+static struct newton newtons_method(double complex x_0, int d){
+    int iteration = 1;
+    double limit = 0.001;
+    double toBig = 10000000000;
+    double n = (double) d;
+    double complex x_i = x_0 - ((cpow(x_0, n) - 1.0) / (n * cpow(x_0, n - 1.0)));
+    printf("test %d\n test2 %f + %fi\n ", iteration, creal(x_i), cimag(x_i));
+    //first condition probably wrong, how do we know the root? (without computing it?).
+    while( (cabs(cpow(x_i, n) - 1.0)  > limit) && (cabs(x_i - x_0) > limit) && (creal(x_i) < toBig) && (cimag(x_i) < toBig) && iteration < 20 ){
+        x_i = x_i - ((cpow(x_i, n) - 1.0) / (n * cpow(x_i, n - 1.0)));
+        iteration++;
+        printf("test %d\n test2 %f + %fi\n ", iteration, creal(x_i), cimag(x_i));
+    }
+    struct newton a = {iteration, 1};
+    return a;
+}
+
+
+
 struct argp_option options[] =
 {
     {"threads", 't', "NUM", 0, "The number of threads to use"},
@@ -67,6 +90,10 @@ int main(int argc, char **argv)
     int i;
 
     printf("t = %d, l = %d, d = %d\n", tvalue, lvalue, dvalue);
-
+    
+    double complex z = -3.0 + 1.0 * I;
+    struct newton n = newtons_method(z, dvalue);
+    printf("iterations = %d\n", n.iterations);
+    
     return 0;
 }
