5351017d5c2863f9a4da890338551099ee9fa335
Andreas Magnusson <andreas.magnusson90@gmail.com>
2017-04-29 18:13:18 +0200

    removed newline print from images

--- assignment2/newton.c
+++ assignment2/newton.c
@@ -237,13 +237,13 @@ int main(int argc, char **argv)
         if (attraction[i] == 0) {
             fprintf(fatt, "%d %d %d ", 0, 0, 0);
         } else {
-            fprintf(fatt, "%d %d %d ", colour[attraction[i]][0],
-                                   colour[attraction[i]][1],
-                                   colour[attraction[i]][2]);
+            fprintf(fatt, "%d %d %d ", colour[attraction[i]-1][0],
+                                   colour[attraction[i]-1][1],
+                                   colour[attraction[i]-1][2]);
         }
-        if((i+1)%l == 0){
-            fprintf(fatt, "\n");
-        }
+        //if((i+1)%l == 0){
+        //    fprintf(fatt, "\n");
+        //}
     }
 
     fclose(fatt);
@@ -254,13 +254,14 @@ int main(int argc, char **argv)
     
     for(int i = 0; i < l*l; i++){
         fprintf(fcon, "%d %d %d ", 255-convergence[i]*col, 255-convergence[i]*col, 255-convergence[i]*col);
-        if((i+1)%l == 0){
-            fprintf(fcon, "\n");
-        }
+        //if((i+1)%l == 0){
+        //    fprintf(fcon, "\n");
+        //}
     }    
 
     fclose(fcon);
-    
+   
+
     free(convergence);
     free(attraction);
 
