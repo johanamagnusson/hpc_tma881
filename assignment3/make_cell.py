8c762360af7ce002d061f5e1422ad889fd47c404
Andreas Magnusson <andreas.magnusson90@gmail.com>
2017-05-06 22:10:17 +0200

    added a make cell script. Added cell to .gitignore.

---
+++ assignment3/make_cell.py
@@ -0,0 +1,34 @@
+import random as rand
+
+NUMBER_OF_POINTS = 10000
+
+def make_string(coordinate):
+    string = ""
+    if coordinate[0] < 0:
+        string += "{0:07.3f} "
+    else:
+        string += "+{0:06.3f} "
+    if coordinate[1] < 0:
+        string += "{1:07.3f} "
+    else:
+        string += "+{1:06.3f} "
+    if coordinate[2] < 0:
+        string += "{2:07.3f}"
+    else:
+        string += "+{2:06.3f}"
+    return string.format(coordinate[0], coordinate[1], coordinate[2])
+
+def main():
+    f = open("cell", "w")
+   
+
+    for i in range(NUMBER_OF_POINTS):
+        coordinate = [
+                -10.0 + 20.0*rand.random(),
+                -10.0 + 20.0*rand.random(),
+                -10.0 + 20.0*rand.random()
+                ]
+        f.write(make_string(coordinate) + "\n")
+    f.close()
+
+main()
