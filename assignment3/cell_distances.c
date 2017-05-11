39a50697c187b08c60f2009ebba24c2ec8a675fe
Andreas Magnusson <andreas.magnusson90@gmail.com>
2017-05-11 17:21:25 +0200

    final program works

--- assignment3/cell_distances.c
+++ assignment3/cell_distances.c
@@ -7,8 +7,6 @@
 
 #define NUMBER_OF_DISTANCES 3464
 
-double **points;
-
 struct arguments
 {
     int t;
@@ -38,38 +36,22 @@ struct argp_option options[] =
 
 struct argp argp = { options, parse_opt, 0, 0 };
 
-double compute_distance(
-        double x1,
-        double x2,
-        double y1,
-        double y2,
-        double z1,
-        double z2
-        )
-{
-    double ret;
-    double dx = x2 - x1;
-    double dy = y2 - y1;
-    double dz = z2 - z1;
-    ret = sqrt(dx*dx + dy*dy + dz*dz);
-    return ret;
-}
-
 int main(int argc, char **argv)
 {
     FILE *cellFile;
-    size_t i, j;
-    int ret, distanceIndex;
+    size_t i, j, new_i, new_j;
+    int ret;
     double distances[NUMBER_OF_DISTANCES];
     int distanceHist[NUMBER_OF_DISTANCES];
     char ch;
     struct arguments arguments;
     unsigned long int numberOfPoints;
+    float **points;
 
     argp_parse (&argp, argc, argv, 0, 0, &arguments); 
     int NUM_THREADS = arguments.t;
 
-    cellFile = fopen("cell", "r");
+    cellFile = fopen("cells", "r");
     numberOfPoints = 0;
     do
     {
@@ -78,13 +60,12 @@ int main(int argc, char **argv)
             numberOfPoints++;
     } while (ch != EOF);
 
-    points =  malloc(numberOfPoints * sizeof(double *));
+    points =  malloc(numberOfPoints * sizeof(float *));
     for (i = 0; i < numberOfPoints; i++)
     {
-        points[i] = malloc(3 * sizeof(double));
+        points[i] = malloc(3 * sizeof(float));
     }
 
-    //distanceHist = (int *) malloc(numberOfDistances * sizeof(int));
     for (i = 0; i < NUMBER_OF_DISTANCES; i++)
     {
         distanceHist[i] = 0;
@@ -94,26 +75,24 @@ int main(int argc, char **argv)
     fseek(cellFile, 0, SEEK_SET);
     for (i = 0; i < numberOfPoints; i++)
     {
-        ret = fscanf(cellFile, "%lf", &points[i][0]);
-        ret = fscanf(cellFile, "%lf", &points[i][1]);
-        ret = fscanf(cellFile, "%lf", &points[i][2]);
+        ret = fscanf(cellFile, "%f", &points[i][0]);
+        ret = fscanf(cellFile, "%f", &points[i][1]);
+        ret = fscanf(cellFile, "%f", &points[i][2]);
     }
     fclose(cellFile);
 
     omp_set_num_threads(NUM_THREADS);
     
-    long int iterations = 0;
-    
-    int threadHist[NUMBER_OF_DISTANCES];
-    #pragma omp parallel private(i,j,threadHist) shared(points,distanceHist)
+    #pragma omp parallel private(i,j,new_i,new_j) shared(points,distanceHist)
     {
-        double dx, dy, dz, distance;
+        float dx, dy, dz, distance;
+        int distanceIndex;
+        int threadHist[NUMBER_OF_DISTANCES];
         for (i = 0; i < NUMBER_OF_DISTANCES; i++)
         {
             threadHist[i] = 0;
         }
         #pragma omp for collapse(2)
-        //printf("Number of distances: %lu\n", numberOfDistances);
         //för den kvadraten i trianglen utan problem 
         for (i = numberOfPoints/2; i < numberOfPoints; i++)
         {
@@ -122,13 +101,13 @@ int main(int argc, char **argv)
                 dx = points[i][0] - points[j][0];
                 dy = points[i][1] - points[j][1];
                 dz = points[i][2] - points[j][2];
-                distance = sqrt(dx*dx + dy*dy + dz*dz);
+                distance = sqrtf(dx*dx + dy*dy + dz*dz);
                 distanceIndex = (int) (distance * 100.0 + 0.5);
                 threadHist[distanceIndex]++;
-                //iterations++;
             }
         }
-        #pragma omp for collapse(2) //reduction(+:iterations)
+
+        #pragma omp for collapse(2)
         //för trianglarna som läggs ihop
         for (i = 0; i < numberOfPoints/2; i++)
         {
@@ -138,26 +117,24 @@ int main(int argc, char **argv)
                     dx = points[i][0] - points[j][0];
                     dy = points[i][1] - points[j][1];
                     dz = points[i][2] - points[j][2];
-                    distance = sqrt(dx*dx + dy*dy + dz*dz);
+                    distance = sqrtf(dx*dx + dy*dy + dz*dz);
                     distanceIndex = (int) (distance * 100.0 + 0.5);
                     threadHist[distanceIndex]++;
-                    //iterations++;
                 } else {
-                    int new_i = numberOfPoints - 1 - i;
-                    int new_j = numberOfPoints - 2 - j;
+                    new_i = numberOfPoints - 1 - i;
+                    new_j = numberOfPoints - 2 - j;
                     dx = points[new_i][0] - points[new_j][0];
                     dy = points[new_i][1] - points[new_j][1];
                     dz = points[new_i][2] - points[new_j][2];
-                    distance = sqrt(dx*dx + dy*dy + dz*dz);
+                    distance = sqrtf(dx*dx + dy*dy + dz*dz);
                     distanceIndex = (int) (distance * 100.0 + 0.5);
                     threadHist[distanceIndex]++;
-                    //iterations++;
                 }
             }
         }
         #pragma omp critical (data_saving)
         {
-            for (int i = 0; i < NUMBER_OF_DISTANCES; i++)
+            for (i = 0; i < NUMBER_OF_DISTANCES; i++)
             {
                 distanceHist[i] += threadHist[i];
             }
@@ -172,16 +149,12 @@ int main(int argc, char **argv)
         }
         
     }
-//    #pragma omp parallel end
-
-    printf("Number of threads: %d\n", NUM_THREADS);
-    printf("Number of points: %lu\n", numberOfPoints);
-    printf("Iterated: %ld\n", iterations); 
 
     for (i = 0; i < numberOfPoints; i++)
     {
         free(points[i]);
     }
+    free(points);
 
     return 0;
 }
